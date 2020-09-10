c1e80cb9aa6b5dcaf5fee99424b9d40503e606fe
Joshua B Gutierrez <joshgm15@gmail.com>
2020-09-10 13:03:31 -0500

    Fixing proxy

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ const path = require('path');
 
 require('dotenv').config();
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-	app.use(express.static('client/build'));
-	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname + '/client/build/index.html'));
-	});
-}
 const middlewares = require('./middlewares');
 const recipe = require('./recipe');
 
@@ -25,6 +19,13 @@ app.use(express.json());
 
 app.use('/', (req, res) => res.json({ message: 'API Workign now' }));
 app.use('/recipe', recipe);
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname + '/client/build/index.html'));
+	});
+}
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
