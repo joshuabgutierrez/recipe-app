3ce6cf7d5afbdc4b932d7a39834e5b3af3d155a4
Joshua B Gutierrez <joshgm15@gmail.com>
2020-09-10 20:43:45 -0500

    Fixing server

--- src/App.js
+++ src/App.js
@@ -14,15 +14,14 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/', recipe);
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-	app.use(express.static(path.join('client/build')));
-	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'client/build/index.html'));
-	});
-}
+app.use(express.static(path.join('client/build')));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
