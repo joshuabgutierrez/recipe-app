73f23d8ceb70c069e8a2ff8f94039ce486d9a732
Joshua B Gutierrez <joshgm15@gmail.com>
2020-09-11 13:27:06 -0500

    Fixed npm scripts

--- src/App.js
+++ src/App.js
@@ -18,10 +18,12 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', recipe);
 
-app.use(express.static(path.join('client/build')));
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
