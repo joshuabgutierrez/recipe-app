02c46fd4a0144cb48192b75eec9106fd72f13c27
Joshua B Gutierrez <joshgm15@gmail.com>
2020-09-10 20:35:41 -0500

    Fixed server

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ app.use(express.json());
 
 app.use('/', recipe);
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'build')));
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+	app.use(express.static(path.join('client/build')));
 	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'build'));
+		res.sendFile(path.join(__dirname, 'client/build/index.html'));
 	});
 }
 
