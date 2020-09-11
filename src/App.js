f4c01166a84305e0c0278b3a12e42789f0f58f49
Joshua B Gutierrez <joshgm15@gmail.com>
2020-09-10 19:40:55 -0500

    Fixing static files

--- src/App.js
+++ src/App.js
@@ -3,20 +3,12 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const path = require('path');
-
-require('dotenv').config();
-
 const middlewares = require('./middlewares');
 const recipe = require('./recipe');
+require('dotenv').config();
 
 const app = express();
 app.set('trust proxy', 1);
-
-if (process.env.NODE_ENV === 'production') {
-	app.use('/*', (req, res) => {
-		res.send(path.join(__dirname, 'client', 'build', 'index.html'));
-	});
-}
 
 app.use(morgan('dev'));
 app.use(helmet());
@@ -24,6 +16,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/', recipe);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'build')));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'build'));
+	});
+}
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
